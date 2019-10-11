c8e25ad3fd70f480d5aa7e66b6fa6a720de3c1ab
Nahku Saidy <nsaidyt-online.de>
2019-10-11 21:57:46 +0200

    added parentheses

--- lexer.py
+++ lexer.py
@@ -8,14 +8,17 @@ tokens = (
     'STRICT_SYMBOL',
     'MACRO_SYMBOL',
     'NT_SYMBOL',
-    'T_SYMBOL',
+#    'T_SYMBOL',
 #    'OR',
     'IDENTIFIER',
     #'PLAIN_TEXT',
-    'COMMENT'
+    'COMMENT',
+    'LEFT_PARENTHESIS',
+    'RIGHT_PARENTHESIS',
+    'OR'
 )
 
-literals = [ '|','<','>','[',']',',','(',')','*']
+literals = ['[',']',',','*']
 
 # Regular expression rules for simple tokens
 t_GRAMMAR_SYMBOL = r'::='
@@ -26,6 +29,9 @@ t_MACRO_SYMBOL = r':::'
 #t_NT_CLOSE_SYMBOL  = r'>'
 #t_OR  = r'\|'
 #t_COMMENT_MARKER = r'%----'
+t_LEFT_PARENTHESIS = r'\('
+t_RIGHT_PARENTHESIS = r'\)'
+t_OR = r'\|'
 
 t_ignore = ' \t'
 
@@ -38,14 +44,15 @@ def t_NT_SYMBOL(t):
     r'<[a-zA-Z_][a-zA-Z_0-9]*>'
     return t
 
-def t_T_SYMBOL(t):
-    #r'..*'
-    return t
+#def t_T_SYMBOL(t):
+#    r'..*'
+#    return t
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+#lex.lex(debug=1)
 lex.lex()
 
 #data = '::=::::==||     ||><bl a:==bla%----'
