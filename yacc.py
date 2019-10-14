6ca99ea65d166c85ee874b6115e7dd81ef5a66b7
Nahku Saidy <nsaidyt-online.de>
2019-10-14 22:34:03 +0200

    added basic yacc

---
+++ yacc.py
@@ -0,0 +1,38 @@
+import ply.yacc as yacc
+
+from lexer import tokens
+
+def p_grammar_expression(p):
+    """
+    grammar_expression: NT_SYMBOL t_GRAMMAR_SYMBOL productions_list
+    """
+    p[0] = p[1] + [p[2]]
+
+def p_productions_list(p):
+    """
+    production_list: production
+    production_list: production_list OR production
+    """
+    if len(p) == 1:
+        p[0] = [p[1]]
+    elif len(p) == 2:
+        p[0] = p[1] + [p[2]]
+
+def p_production(p):
+    """
+    production: NT_SYMBOL
+    production: T_SYMBOL
+    production: production NT_SYMBOL
+    production: production T_SYMBOL
+    """
+
+parser = yacc.yacc()
+
+while True:
+   #try:
+   #    s = raw_input('calc > ')
+   #except EOFError:
+   #    break
+   #if not s: continue
+   result = parser.parse()
+   print(result)
