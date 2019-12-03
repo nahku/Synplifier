d0c51dfb77a150815b8b549fcfbb1e4b129d5da6
nahku <nsaidy@t-online.de>
2019-12-03 17:33:20 +0100

    added gui menu bar

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 import TreeBuilder
-
+import view
 if __name__== "__main__":
   #parser = TPTPParser()
   #test_result = parser.run()
   #print(test_result)
   treeBuilder = TreeBuilder.TPTPTreeBuilder('TPTP_BNF_NEW.txt','disable_rules.txt')
+  view.scrollbar(treeBuilder)
   #treeBuilder.print_ordered_rules_from_graph()
