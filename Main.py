92b3e75738d6ef6e6d2ae84c3f5e0d891ce875d9
nahku <nsaidy@t-online.de>
2019-12-03 13:38:27 +0100

    added main file

---
+++ Main.py
@@ -0,0 +1,7 @@
+import TreeBuilder
+
+if __name__== "__main__":
+  #parser = TPTPParser()
+  #test_result = parser.run()
+  #print(test_result)
+  treeBuilder = TreeBuilder.TPTPTreeBuilder('TPTP_BNF_NEW.txt','disable_rules.txt')
