3ff59c97575d4d96eb1d2a097f6824cfd441d79f
nahku <nsaidy@t-online.de>
2019-12-09 13:28:35 +0100

    fixed tests

--- lexerTest.py
+++ lexerTest.py
@@ -16,48 +16,56 @@ class TestExpressions(unittest.TestCase):
 
     def test_grammar_rule(self):
         lexer = lex.TPTPLexer().lexer
-        lexer.input('::=')
+        lexer.input('<TPTP_TEST> ::=')
         type = True
+        i = 0
         while 1:
             tok = lexer.token()
             if not tok: break  # No more input
-            if tok.type != 'GRAMMAR_SYMBOL':
-                type = False
+            if (i==1):
+                if tok.type != 'GRAMMAR_SYMBOL':
+                    type = False
+            i+1
         self.assertEqual(True, type)
 
     def test_token_rule(self):
         lexer = lex.TPTPLexer().lexer
-        lexer.input('::-')
+        lexer.input('<TPTP_TEST> ::-')
         type = True
+        i = 0
         while 1:
             tok = lexer.token()
             if not tok: break  # No more input
-            if tok.type != 'TOKEN_SYMBOL':
-                type = False
+            if (i == 1):
+                if tok.type != 'TOKEN_SYMBOL':
+                    type = False
+            i + 1
         self.assertEqual(True, type)
 
     def test_strict_rule(self):
         lexer = lex.TPTPLexer().lexer
-        lexer.input(':==')
+        lexer.input('<TPTP_Test> :==')
         type = True
         i = 0
         while 1:
             tok = lexer.token()
             if not tok: break  # No more input
-            if tok.type != 'STRICT_SYMBOL':
-                type = False
+            if(i==1):
+                if tok.type != 'STRICT_SYMBOL':
+                    type = False
         self.assertEqual(True, type)
 
     def test_macro_rule(self):
         lexer = lex.TPTPLexer().lexer
-        lexer.input(':::')
+        lexer.input('<TPTP_Test> :::')
         type = True
         i = 0
         while 1:
             tok = lexer.token()
             if not tok: break  # No more input
-            if tok.type != r'MACRO_SYMBOL':
-                type = False
+            if(i == 1):
+                if tok.type != r'MACRO_SYMBOL':
+                    type = False
             i += 1
         self.assertEqual(True, type)
 
