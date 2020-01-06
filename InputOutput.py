b17346b74a744759b93b2ec1d63e828d35a1a376
Nahku Saidy <nsaidyt-online.de>
2020-01-06 11:12:33 +0100

    Refactored InputOutput

--- InputOutput.py
+++ InputOutput.py
@@ -6,22 +6,6 @@ import GraphBuilder
 PrintListEntry = namedtuple("PrintListEntry", ["position", "line_list"])
 INDENT_LENGTH = 20
 
-# def print_tree( node, level):
-#     if level == 0:
-#         print_tree_nt(node.value, level)
-#     if len(node.children) > 0:
-#         if len(node.children[0]) >= 0:
-#             for i in node.children:
-#                 print("")
-#                 for j in i:
-#                     print_tree_nt(j.value, level+1)
-#                     print_tree(j, level+2)
-#
-# def print_tree_nt( nt_name, level):
-#     for i in range(0,level):
-#         print("  ",end = "")
-#     print(nt_name, end = "")
-
 def print_rules_from_rules_list(rules_list):
     for i in rules_list.list:
         if(isinstance(i,yacc.MACRO_EXPRESSION)|isinstance(i,yacc.STRICT_EXPRESSION)|isinstance(i,yacc.GRAMMAR_EXPRESSION)|isinstance(i,yacc.TOKEN_EXPRESSION)):
@@ -82,7 +66,6 @@ def print_rule_from_nt_node( node):
     print_productions_list(node.productions_list)
     print("")
 
-
 def print_ordered_rules_from_graph(start_node):
     visited = {}
     print_list = []
@@ -103,6 +86,92 @@ def print_expression( expression):
     elif (isinstance(expression, yacc.MACRO_EXPRESSION)):
         print_wo_newline(" ::: ")
     print_productions_list(expression.productions_list)
+
+def print_production( production):
+    for i in production.list:
+        if(isinstance(i, yacc.PRODUCTION)):
+            if (i.productionProperty == yacc.ProductionProperty.NONE):
+                print_production(i)
+            elif(i.productionProperty == yacc.ProductionProperty.REPETITION):
+                print_wo_newline("(")
+                print_production(i)
+                print_wo_newline(")")
+                print_wo_newline("*")
+            elif(i.productionProperty == yacc.ProductionProperty.OPTIONAL):
+                print_wo_newline("[")
+                print_production(i)
+                print_wo_newline("]")
+            elif (i.productionProperty == yacc.ProductionProperty.XOR):
+                print_wo_newline("(")
+                print_production(i)
+                print_wo_newline(")")
+        elif(isinstance(i, yacc.XOR_PRODUCTIONS_LIST)):
+            print_xor_productions_list(i)
+        elif (isinstance(i, yacc.PRODUCTION_ELEMENT)):
+            print_production_element(i)
+
+def print_xor_productions_list(xor_productions_list):
+    print_wo_newline("(")
+    print_productions_list(xor_productions_list)
+    print_wo_newline(")")
+
+def print_production_element(production_element):
+    if (production_element.productionProperty == yacc.ProductionProperty.NONE):
+        print_symbol(production_element.name)
+    elif (production_element.productionProperty == yacc.ProductionProperty.REPETITION):
+        print_symbol(production_element.name)
+        print_wo_newline("*")
+    elif (production_element.productionProperty == yacc.ProductionProperty.OPTIONAL):
+        print_wo_newline("[")
+        print_symbol(production_element.name)
+        print_wo_newline("]")
+    elif (production_element.productionProperty == yacc.ProductionProperty.XOR):
+        print_symbol(production_element.name)
+        print_wo_newline(" |")
+
+def print_symbol( symbol):
+    if isinstance(symbol,yacc.T_SYMBOL):
+        if (symbol.property == yacc.ProductionProperty.NONE):
+            print_wo_newline(symbol.value)
+        elif (symbol.property == yacc.ProductionProperty.REPETITION):
+            print_wo_newline(symbol.value)
+            print_wo_newline("*")
+        elif (symbol.property == yacc.ProductionProperty.OPTIONAL):
+            print_wo_newline("[")
+            print_wo_newline(symbol.value)
+            print_wo_newline("]")
+        elif (symbol.property == yacc.ProductionProperty.XOR):
+            print_wo_newline("(")
+            print_wo_newline(symbol.value)
+            print_wo_newline(")")
+    elif(isinstance(symbol,yacc.NT_SYMBOL)):
+        print_wo_newline(symbol.value)
+    elif(isinstance(symbol,list)):
+        print_wo_newline(symbol[0].value)  #only first because all list elements have the same name
+
+def print_productions_list(productions_list):
+    length = len(productions_list.list)
+    j = 1
+    for i in productions_list.list:
+        print_production(i)
+        if(j<length):
+            print_wo_newline(" | ")
+        j = j + 1
+
+def print_comment_block(comment_block: yacc.COMMENT_BLOCK):
+    """Prints all lines of a COMMENT_BLOCK object to console.
+
+    :param comment_block: The comment block that should be printed.
+    """
+    for i in comment_block.list:
+        print(i)
+
+def print_wo_newline(string: str):
+    """Prints a string without "\n at the end."
+
+    :param string: The string that shold be printed.
+    """
+    print(string, end = '')
 
 def get_production_string(production):
     production_string = ""
@@ -129,40 +198,12 @@ def get_production_string(production):
             production_string += get_production_element_string(i)
     return production_string
 
-def print_production( production):
-    for i in production.list:
-        if(isinstance(i, yacc.PRODUCTION)):
-            if (i.productionProperty == yacc.ProductionProperty.NONE):
-                print_production(i)
-            elif(i.productionProperty == yacc.ProductionProperty.REPETITION):
-                print_wo_newline("(")
-                print_production(i)
-                print_wo_newline(")")
-                print_wo_newline("*")
-            elif(i.productionProperty == yacc.ProductionProperty.OPTIONAL):
-                print_wo_newline("[")
-                print_production(i)
-                print_wo_newline("]")
-            elif (i.productionProperty == yacc.ProductionProperty.XOR):
-                print_wo_newline("(")
-                print_production(i)
-                print_wo_newline(")")
-        elif(isinstance(i, yacc.XOR_PRODUCTIONS_LIST)):
-            print_xor_productions_list(i)
-        elif (isinstance(i, yacc.PRODUCTION_ELEMENT)):
-            print_production_element(i)
-
 def get_xor_productions_list_string(xor_productions_list):
     xor_productions_list_string = ""
     xor_productions_list_string += "("
     xor_productions_list_string += get_productions_list_string(xor_productions_list)
     xor_productions_list_string += ")"
     return xor_productions_list_string
-
-def print_xor_productions_list(xor_productions_list):
-    print_wo_newline("(")
-    print_productions_list(xor_productions_list)
-    print_wo_newline(")")
 
 def get_production_element_string(production_element):
     production_element_string = ""
@@ -179,20 +220,6 @@ def get_production_element_string(production_element):
         production_element_string += get_symbol_string(production_element.name)
         production_element_string += " | "
     return production_element_string
-
-def print_production_element(production_element):
-    if (production_element.productionProperty == yacc.ProductionProperty.NONE):
-        print_symbol(production_element.name)
-    elif (production_element.productionProperty == yacc.ProductionProperty.REPETITION):
-        print_symbol(production_element.name)
-        print_wo_newline("*")
-    elif (production_element.productionProperty == yacc.ProductionProperty.OPTIONAL):
-        print_wo_newline("[")
-        print_symbol(production_element.name)
-        print_wo_newline("]")
-    elif (production_element.productionProperty == yacc.ProductionProperty.XOR):
-        print_symbol(production_element.name)
-        print_wo_newline(" |")
 
 def get_symbol_string(symbol):
     symbol_string = ""
@@ -216,26 +243,6 @@ def get_symbol_string(symbol):
         symbol_string += symbol[0].value #only first because all list elements have the same name
     return symbol_string
 
-def print_symbol( symbol):
-    if isinstance(symbol,yacc.T_SYMBOL):
-        if (symbol.property == yacc.ProductionProperty.NONE):
-            print_wo_newline(symbol.value)
-        elif (symbol.property == yacc.ProductionProperty.REPETITION):
-            print_wo_newline(symbol.value)
-            print_wo_newline("*")
-        elif (symbol.property == yacc.ProductionProperty.OPTIONAL):
-            print_wo_newline("[")
-            print_wo_newline(symbol.value)
-            print_wo_newline("]")
-        elif (symbol.property == yacc.ProductionProperty.XOR):
-            print_wo_newline("(")
-            print_wo_newline(symbol.value)
-            print_wo_newline(")")
-    elif(isinstance(symbol,yacc.NT_SYMBOL)):
-        print_wo_newline(symbol.value)
-    elif(isinstance(symbol,list)):
-        print_wo_newline(symbol[0].value)  #only first because all list elements have the same name
-
 def get_productions_list_string(productions_list):
     productions_list_string = ""
     length = len(productions_list.list)
@@ -246,27 +253,3 @@ def get_productions_list_string(productions_list):
             productions_list_string += " | "
         j = j + 1
     return productions_list_string
-
-def print_productions_list(productions_list):
-    length = len(productions_list.list)
-    j = 1
-    for i in productions_list.list:
-        print_production(i)
-        if(j<length):
-            print_wo_newline(" | ")
-        j = j + 1
-
-def print_comment_block(comment_block: yacc.COMMENT_BLOCK):
-    """Prints all lines of a COMMENT_BLOCK object to console.
-
-    :param comment_block: The comment block that should be printed.
-    """
-    for i in comment_block.list:
-        print(i)
-
-def print_wo_newline(string: str):
-    """Prints a string without "\n at the end."
-
-    :param string: The string that shold be printed.
-    """
-    print(string, end = '')
