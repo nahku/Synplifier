f5f913f014b1bb2885d599a54e59dc9f218c6cb9
nahku <nsaidy@t-online.de>
2020-03-21 01:55:48 +0100

    refactored console

--- Console.py
+++ Console.py
@@ -11,21 +11,17 @@ class Console:
                                         disable_rules_string=Input.read_text_from_file(args.control))
         start_node = graphBuilder.nodes_dictionary.get(Node("<start_symbol>", RuleType.GRAMMAR))
         if start_node:
-            if args.output is not None:
-                output_path = args.output
-            else:
-                output_path = "output.txt"
-            Output.save_ordered_rules_from_graph(output_path, start_node)
+            Output.save_ordered_rules_from_graph(args.output, start_node)
 
     def __init__(self):
         self.argument_parser = argparse.ArgumentParser(
             description='Extract sublanguage using TPTP syntax file and a control file')
         self.argument_parser.add_argument('-g', '--grammar', metavar='', type=str, required=True,
-                                          help='path of the TPTP language grammar file')
+                                          help='path of the TPTP syntax file')
         self.argument_parser.add_argument('-c', '--control', metavar='', type=str, required=True,
                                           help='path of the control file')
         self.argument_parser.add_argument('-o', '--output', metavar='', type=str, required=False,
-                                          help='optional output file name (default output.txt)')
+                                          help='optional output file name (default output.txt)', default= "output.txt")
 
 
 if __name__ == "__main__":
